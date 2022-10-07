18c208a78a490f6489b870afc57844d8aba28593
Marcus Ross <marcus.ross@zahlenhelfer.de>
2022-10-07 13:39:24 +0200

    added version contraints for aws provider

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3.71"
+    }
+  }
+  required_version = ">=1.0"
+}
+
 provider "aws" {
   region = "eu-central-1"
 }
@@ -5,6 +15,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-05ff5eaef6149df49"
   instance_type = "t2.micro"
+  monitoring    = true
   tags = {
     Name = "ExampleAppServerInstance"
   }
